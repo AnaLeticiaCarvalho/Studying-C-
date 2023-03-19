ba9b88bba05d05031c678d488c6275272ece827d
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-03-19 11:55:40 -0300

    foreach

--- Program.cs
+++ Program.cs
@@ -27,9 +27,15 @@ internal class Program
         arrayInteiros[1] = 64;
         arrayInteiros[2] = 50;
 
-        for (int contador = 0; contador < arrayInteiros.Length; contador++)
+        /*for (int contador = 0; contador < arrayInteiros.Length; contador++)
         {
             Console.WriteLine($"Posição N* {contador} - {arrayInteiros[contador]}");
+        }
+        */
+
+        foreach(int valor in arrayInteiros)
+        {
+            Console.WriteLine(valor);
         }
 
 
